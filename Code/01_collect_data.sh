bf66798506070ddac6116a54be9dcdabc3d887af
Benjamin Lorentz <bjl34716@uga.edu>
2022-03-29 15:08:04 -0400

    add unzip chunk

--- Code/01_collect_data.sh
+++ Code/01_collect_data.sh
@@ -5,3 +5,8 @@ mkdir proteomes
 cd proteomes
 wget http://ftp.ensembl.org/pub/release-105/fasta/homo_sapiens/pep/Homo_sapiens.GRCh38.pep.all.fa.gz
 
+# Uncompress the data
+for f in *.gz
+do 
+    gunzip $f
+done;

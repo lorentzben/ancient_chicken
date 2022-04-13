3bb96726fa487ad63364b5c12f9cd863d0405211
Benjamin Lorentz <bjl34716@uga.edu>
2022-04-13 10:38:56 -0400

    dos2unix and new command

--- Code/02_call_orthofinder.sh
+++ Code/02_call_orthofinder.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
 # Call orthofinder 
-orthofinder -f proteomes/primary_transcripts/ -S blast -T iqtree -t 16
+orthofinder -f proteomes/primary_transcripts/ -S blast -M msa -A mafft -T iqtree -t 16

73488b77ea5f6ab9dcf753663ca4b64a45d4ab3b
Benjamin Lorentz <bjl34716@uga.edu>
2022-04-13 10:55:29 -0400

    fix iqtree dependency

--- Code/slurm_submission.sh
+++ Code/slurm_submission.sh
@@ -12,7 +12,7 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 ml BLAST+/2.12.0-gompi-2020b
-ml IQ-TREE/2.2.0-GCC-10.2.0-nompi
+ml IQ-TREE/1.6.12-foss-2019b
 ml OrthoFinder/2.5.2-foss-2019b-Python-3.7.4
 bash 02_call_orthofinder.sh
 

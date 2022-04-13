2449ba6d7a0a84bc03e8ea2aeb622aa5b01e8885
Benjamin Lorentz <bjl34716@uga.edu>
2022-04-13 10:48:34 -0400

    use orthofinder on the cluster

--- Code/slurm_submission.sh
+++ Code/slurm_submission.sh
@@ -13,5 +13,6 @@
 
 ml BLAST+/2.12.0-gompi-2020b
 ml IQ-TREE/2.2.0-GCC-10.2.0-nompi
-singularity run docker://lorentzb/orthofinder bash 02_call_orthofinder.sh
+ml OrthoFinder/2.5.2-foss-2019b-Python-3.7.4
+bash 02_call_orthofinder.sh
 

47de37c33f1bc748dc0504bfd1fa1a019b6f8c4a
Benjamin Lorentz <bjl34716@uga.edu>
2022-05-24 14:29:58 -0400

    add dog and tropical frog

--- Code/01_collect_data.sh
+++ Code/01_collect_data.sh
@@ -11,6 +11,9 @@ wget http://ftp.ensembl.org/pub/release-105/fasta/terrapene_carolina_triunguis/p
 wget http://ftp.ensembl.org/pub/release-105/fasta/prolemur_simus/pep/Prolemur_simus.Prosim_1.0.pep.all.fa.gz -O Prolemur_simus.fa.gz
 wget http://ftp.ensembl.org/pub/release-105/fasta/gorilla_gorilla/pep/Gorilla_gorilla.gorGor4.pep.all.fa.gz -O Gorilla_gorilla.fa.gz
 wget http://ftp.ensembl.org/pub/release-105/fasta/phascolarctos_cinereus/pep/Phascolarctos_cinereus.phaCin_unsw_v4.1.pep.all.fa.gz -O Phascolarctos_cinereus.fa.gz
+wget http://ftp.ensembl.org/pub/release-106/fasta/canis_lupus_familiaris/pep/Canis_lupus_familiaris.ROS_Cfam_1.0.pep.all.fa.gz -O Canis_lupus_familiaris.fa.gz
+wget http://ftp.ensembl.org/pub/release-106/fasta/xenopus_tropicalis/pep/Xenopus_tropicalis.Xenopus_tropicalis_v9.1.pep.all.fa.gz -O xenopus_tropicalis.fa.gz
+
 
 # Uncompress the data
 for f in *.gz
